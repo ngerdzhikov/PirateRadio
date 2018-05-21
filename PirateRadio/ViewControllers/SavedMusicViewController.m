dd798980dcf4b1f1f70edff9edbf0c98f43cd33f
Nikola Gerdzhikov <gerdjikov17@abv.bg>
2018-05-21 14:44:40 +0300

    Adds smooth resize transition to musicControllerView
    Also removes some debug NSLogs from AppDelegate
    Also now changes the indexOfLastPlayedSong when deleting a song (it was a bug).

--- PirateRadio/ViewControllers/SavedMusicViewController.m
+++ PirateRadio/ViewControllers/SavedMusicViewController.m
@@ -19,7 +19,7 @@
 @property (strong, nonatomic) NSMutableArray<LocalSongModel *> *songs;
 @property (strong, nonatomic) AVPlayer *player;
 @property (strong, nonatomic) IBOutlet MusicControllerView *musicControllerView;
-@property (strong, nonatomic) NSIndexPath *indexPathOfLastTappedMediaPlaybackButton;
+@property (strong, nonatomic) NSIndexPath *indexPathOfLastPlayedSong;
 
 @end
 
@@ -45,24 +45,22 @@
     double time = CMTimeGetSeconds(self.player.currentTime);
     double duration = CMTimeGetSeconds(self.player.currentItem.duration);
     [self.musicControllerView.songTimeProgress setProgress:(time/duration) animated:NO];
-    SavedMusicTableViewCell *cell = [self.tableView cellForRowAtIndexPath:self.indexPathOfLastTappedMediaPlaybackButton];
+    SavedMusicTableViewCell *cell = [self.tableView cellForRowAtIndexPath:self.indexPathOfLastPlayedSong];
     cell.circleProgressBar.value = time / duration * 100;
 }
 
 - (void)configureMusicControllerView {
     self.musicControllerView.songTimeProgress.progress = 0.0f;
-    [self.musicControllerView setCenter:(CGPointMake(self.view.frame.size.width / 2, (self.view.frame.size.height + 56)))];
+    [self.musicControllerView setCenter:(CGPointMake(self.view.frame.size.width / 2, (self.view.frame.size.height + 65)))];
     UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(onMusicControllerPan:)];
     [self.musicControllerView addGestureRecognizer:pan];
     [self.navigationController.view addSubview:self.musicControllerView];
-//    NSLayoutConstraint *bottom = [NSLayoutConstraint constraintWithItem:self.musicControllerView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.navigationController attribute:NSLayoutAttributeBottom multiplier:1.0f constant:(self.view.frame.size.height + (self.musicControllerView.frame.size.height * 1/6))];
-//    [self.musicControllerView addConstraint:bottom];
     [self loadFirstItemForPlayerAtBeginning];
 }
 
 - (void)loadFirstItemForPlayerAtBeginning {
-    self.indexPathOfLastTappedMediaPlaybackButton = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self replacePlayerSongWithSongAtIndexPath:self.indexPathOfLastTappedMediaPlaybackButton];
+    self.indexPathOfLastPlayedSong = [NSIndexPath indexPathForRow:0 inSection:0];
+    [self replacePlayerSongWithSongAtIndexPath:self.indexPathOfLastPlayedSong];
     [self setMusicPlayerSongNameAndImage];
 }
 
@@ -99,110 +97,26 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     SavedMusicTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"savedMusicCell" forIndexPath:indexPath];
     LocalSongModel *song = self.songs[indexPath.row];
-    cell.musicTitle.text = [[song.artistName stringByAppendingString:@" - "] stringByAppendingString:song.songTitle];
+    if ([song.artistName isEqualToString:@"Uknown artist"]) {
+        cell.musicTitle.text = song.songTitle;
+    }
+    else {
+        cell.musicTitle.text = [[song.artistName stringByAppendingString:@" - "] stringByAppendingString:song.songTitle];
+    }
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(cellPlayButtonTap:)];
     [cell.circleProgressBar addGestureRecognizer:tap];
 
     return cell;
 }
 
-- (void)cellPlayButtonTap:(UITapGestureRecognizer *)recognizer {
-    
-    CGPoint touchLocation = [recognizer locationInView:self.tableView];
-    NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:touchLocation];
-    
-    AVURLAsset *assetToPlay = [[AVURLAsset alloc] initWithURL:self.songs[indexPath.row].localSongURL options:nil];
-    AVPlayerItem *itemToPlay = [AVPlayerItem playerItemWithAsset:assetToPlay];
-    
-    SavedMusicTableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
-    
-    if ([indexPath isEqual:self.indexPathOfLastTappedMediaPlaybackButton]) {
-        if (self.player.timeControlStatus == AVPlayerTimeControlStatusPlaying) {
-            [NSNotificationCenter.defaultCenter postNotificationName:NOTIFICATION_PAUSE_BUTTON_PRESSED object:nil userInfo:@{@"cell" : cell}];
-        }
-        else {
-            [NSNotificationCenter.defaultCenter postNotificationName:NOTIFICATION_PLAY_BUTTON_PRESSED object:nil userInfo:@{@"cell" : cell}];
-        }
-    }
-    else {
-        [NSNotificationCenter.defaultCenter postNotificationName:NOTIFICATION_PLAY_BUTTON_PRESSED object:nil userInfo:@{@"cell" : cell}];
-        [self.player replaceCurrentItemWithPlayerItem:itemToPlay];
-    }
-    
-    self.indexPathOfLastTappedMediaPlaybackButton = indexPath;
-    [self setMusicPlayerSongNameAndImage];
-}
-
-#pragma mark - music view button actions
-
-- (IBAction)musicControllerPlayBtnTap:(id)sender {
-    if (self.player.timeControlStatus == AVPlayerTimeControlStatusPlaying) {
-        [NSNotificationCenter.defaultCenter postNotificationName:NOTIFICATION_PAUSE_BUTTON_PRESSED object:nil userInfo:@{@"cell" : [self.tableView cellForRowAtIndexPath:self.indexPathOfLastTappedMediaPlaybackButton]}];
-    }
-    else {
-        [NSNotificationCenter.defaultCenter postNotificationName:NOTIFICATION_PLAY_BUTTON_PRESSED object:nil userInfo:@{@"cell" : [self.tableView cellForRowAtIndexPath:self.indexPathOfLastTappedMediaPlaybackButton]}];
-    }
-}
-
-- (IBAction)previousBtnTap:(id)sender {
-    NSIndexPath *indexPath;
-    if ((self.indexPathOfLastTappedMediaPlaybackButton.row - 1) < 0) {
-        indexPath = [NSIndexPath indexPathForRow:self.songs.count - 1 inSection:self.indexPathOfLastTappedMediaPlaybackButton.section];
-        [self replacePlayerSongWithSongAtIndexPath:indexPath];
-    }
-    else {
-        indexPath = [NSIndexPath indexPathForRow:(self.indexPathOfLastTappedMediaPlaybackButton.row - 1) inSection:self.indexPathOfLastTappedMediaPlaybackButton.section];
-        [self replacePlayerSongWithSongAtIndexPath:indexPath];
-    }
-    [NSNotificationCenter.defaultCenter postNotificationName:NOTIFICATION_PLAY_BUTTON_PRESSED object:nil userInfo:@{@"cell" : [self.tableView cellForRowAtIndexPath:indexPath]}];
-    self.indexPathOfLastTappedMediaPlaybackButton = indexPath;
-    [self setMusicPlayerSongNameAndImage];
-}
-
-- (IBAction)nextBtnTap:(id)sender {
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:(self.indexPathOfLastTappedMediaPlaybackButton.row + 1) % self.songs.count inSection:self.indexPathOfLastTappedMediaPlaybackButton.section];
-    [self replacePlayerSongWithSongAtIndexPath:indexPath];
-    [NSNotificationCenter.defaultCenter postNotificationName:NOTIFICATION_PLAY_BUTTON_PRESSED object:nil userInfo:@{@"cell" : [self.tableView cellForRowAtIndexPath:indexPath]}];
-    self.indexPathOfLastTappedMediaPlaybackButton = indexPath;
-    [self setMusicPlayerSongNameAndImage];
-}
-
-
-- (void)onMusicControllerPan:(UIPanGestureRecognizer *)recognizer {
-    CGFloat velocityY = (0.2*[recognizer velocityInView:self.view].y);
-    CGPoint translatedPoint = [recognizer translationInView:recognizer.view.superview];
-    CGFloat animationDuration = (ABS(velocityY)*.0002)+.2;
-    
-    [UIView beginAnimations:nil context:NULL];
-    [UIView setAnimationDuration:animationDuration];
-    [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
-//    if (translatedPoint.y < 0) {
-//        [recognizer.view setCenter:CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height + translatedPoint.y)];
-//    }
-//    else {
-//        [recognizer.view setCenter:CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height - 1/translatedPoint.y)];
-//    }
-    if (translatedPoint.y < -100) {
-        [recognizer.view setCenter:CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height-0.5*self.musicControllerView.frame.size.height)];
-    }
-    if (translatedPoint.y > 100) {
-        [recognizer.view setCenter:CGPointMake(self.view.frame.size.width/2, (self.view.frame.size.height + (self.musicControllerView.frame.size.height * 1/6)))];
-    }
-    [UIView commitAnimations];
-}
-
-- (void)replacePlayerSongWithSongAtIndexPath:(NSIndexPath *)indexPath {
-    AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:self.songs[indexPath.row].localSongURL options:nil];
-    AVPlayerItem *item = [AVPlayerItem playerItemWithAsset:asset];
-    [self.player replaceCurrentItemWithPlayerItem:item];
-    self.indexPathOfLastTappedMediaPlaybackButton = indexPath;
-    [self setMusicPlayerSongNameAndImage];
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 44.0f;
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    LocalSongModel *song = self.songs[self.indexPathOfLastTappedMediaPlaybackButton.row];
+    LocalSongModel *song = self.songs[self.indexPathOfLastPlayedSong.row];
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        if (indexPath != self.indexPathOfLastTappedMediaPlaybackButton){
+        if (indexPath != self.indexPathOfLastPlayedSong){
             NSError *error;
             [NSFileManager.defaultManager removeItemAtURL:song.localSongURL error:&error];
             if (error) {
@@ -211,6 +125,11 @@
             else {
                 [self.songs removeObjectAtIndex:indexPath.row];
                 [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+                if (indexPath.section == self.indexPathOfLastPlayedSong.section) {
+                    if (indexPath.row < self.indexPathOfLastPlayedSong.row) {
+                        self.indexPathOfLastPlayedSong = [NSIndexPath indexPathForRow:(self.indexPathOfLastPlayedSong.row - 1) inSection:self.indexPathOfLastPlayedSong.section];
+                    }
+                }
                 [self.tableView reloadData];
             }
         }
@@ -225,6 +144,104 @@
     }
 }
 
+- (void)cellPlayButtonTap:(UITapGestureRecognizer *)recognizer {
+    
+    CGPoint touchLocation = [recognizer locationInView:self.tableView];
+    NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:touchLocation];
+    
+    AVURLAsset *assetToPlay = [[AVURLAsset alloc] initWithURL:self.songs[indexPath.row].localSongURL options:nil];
+    AVPlayerItem *itemToPlay = [AVPlayerItem playerItemWithAsset:assetToPlay];
+    
+    SavedMusicTableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+    
+    if ([indexPath isEqual:self.indexPathOfLastPlayedSong]) {
+        if (self.player.timeControlStatus == AVPlayerTimeControlStatusPlaying) {
+            [NSNotificationCenter.defaultCenter postNotificationName:NOTIFICATION_PAUSE_BUTTON_PRESSED object:nil userInfo:@{@"cell" : cell}];
+        }
+        else {
+            [NSNotificationCenter.defaultCenter postNotificationName:NOTIFICATION_PLAY_BUTTON_PRESSED object:nil userInfo:@{@"cell" : cell}];
+        }
+    }
+    else {
+        [NSNotificationCenter.defaultCenter postNotificationName:NOTIFICATION_PLAY_BUTTON_PRESSED object:nil userInfo:@{@"cell" : cell}];
+        [self.player replaceCurrentItemWithPlayerItem:itemToPlay];
+    }
+    
+    self.indexPathOfLastPlayedSong = indexPath;
+    [self setMusicPlayerSongNameAndImage];
+}
+
+#pragma mark - music view button actions
+
+- (IBAction)musicControllerPlayBtnTap:(id)sender {
+    if (self.player.timeControlStatus == AVPlayerTimeControlStatusPlaying) {
+        [NSNotificationCenter.defaultCenter postNotificationName:NOTIFICATION_PAUSE_BUTTON_PRESSED object:nil userInfo:@{@"cell" : [self.tableView cellForRowAtIndexPath:self.indexPathOfLastPlayedSong]}];
+    }
+    else {
+        [NSNotificationCenter.defaultCenter postNotificationName:NOTIFICATION_PLAY_BUTTON_PRESSED object:nil userInfo:@{@"cell" : [self.tableView cellForRowAtIndexPath:self.indexPathOfLastPlayedSong]}];
+    }
+}
+
+- (IBAction)previousBtnTap:(id)sender {
+    NSIndexPath *indexPath;
+    if ((self.indexPathOfLastPlayedSong.row - 1) < 0) {
+        indexPath = [NSIndexPath indexPathForRow:self.songs.count - 1 inSection:self.indexPathOfLastPlayedSong.section];
+        [self replacePlayerSongWithSongAtIndexPath:indexPath];
+    }
+    else {
+        indexPath = [NSIndexPath indexPathForRow:(self.indexPathOfLastPlayedSong.row - 1) inSection:self.indexPathOfLastPlayedSong.section];
+        [self replacePlayerSongWithSongAtIndexPath:indexPath];
+    }
+    [NSNotificationCenter.defaultCenter postNotificationName:NOTIFICATION_PLAY_BUTTON_PRESSED object:nil userInfo:@{@"cell" : [self.tableView cellForRowAtIndexPath:indexPath]}];
+    self.indexPathOfLastPlayedSong = indexPath;
+    [self setMusicPlayerSongNameAndImage];
+}
+
+- (IBAction)nextBtnTap:(id)sender {
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:(self.indexPathOfLastPlayedSong.row + 1) % self.songs.count inSection:self.indexPathOfLastPlayedSong.section];
+    [self replacePlayerSongWithSongAtIndexPath:indexPath];
+    [NSNotificationCenter.defaultCenter postNotificationName:NOTIFICATION_PLAY_BUTTON_PRESSED object:nil userInfo:@{@"cell" : [self.tableView cellForRowAtIndexPath:indexPath]}];
+    self.indexPathOfLastPlayedSong = indexPath;
+    [self setMusicPlayerSongNameAndImage];
+}
+
+
+- (void)onMusicControllerPan:(UIPanGestureRecognizer *)recognizer {
+    CGFloat velocityY = (0.2*[recognizer velocityInView:self.view].y);
+    CGPoint translatedPoint = [recognizer translationInView:recognizer.view.superview];
+    CGFloat animationDuration = (ABS(velocityY)*.0002)+.2;
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:animationDuration];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
+    
+    CGPoint newCenter = CGPointMake(self.view.frame.size.width/2, recognizer.view.center.y + translatedPoint.y);
+    
+    if (newCenter.y >= ((self.view.frame.size.height) - self.musicControllerView.frame.size.height/2) && newCenter.y <= (self.view.frame.size.height + 65)) {
+        recognizer.view.center = newCenter;
+    }
+    
+    if (recognizer.state == UIGestureRecognizerStateEnded) {
+        if (recognizer.view.center.y > ((self.view.frame.size.height + 100) - self.musicControllerView.frame.size.height/2)) {
+            recognizer.view.center = CGPointMake(self.view.frame.size.width/2, (self.view.frame.size.height + 65));
+        }
+        else {
+            recognizer.view.center = CGPointMake(self.view.frame.size.width/2, (self.view.frame.size.height - (self.musicControllerView.frame.size.height/2)));
+        }
+    }
+    [recognizer setTranslation:CGPointMake(0, 0) inView:self.view];
+    
+    [UIView commitAnimations];
+}
+
+- (void)replacePlayerSongWithSongAtIndexPath:(NSIndexPath *)indexPath {
+    AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:self.songs[indexPath.row].localSongURL options:nil];
+    AVPlayerItem *item = [AVPlayerItem playerItemWithAsset:asset];
+    [self.player replaceCurrentItemWithPlayerItem:item];
+    self.indexPathOfLastPlayedSong = indexPath;
+    [self setMusicPlayerSongNameAndImage];
+}
+
 - (void)playButtonTap:(NSNotification *)notification {
     [self.player play];
     [self.musicControllerView.playButton setTitle:BUTTON_TITLE_PAUSE_STRING forState:UIControlStateNormal];
@@ -236,7 +253,7 @@
 }
 
 - (void)setMusicPlayerSongNameAndImage {
-    LocalSongModel *song = self.songs[self.indexPathOfLastTappedMediaPlaybackButton.row];
+    LocalSongModel *song = self.songs[self.indexPathOfLastPlayedSong.row];
     self.musicControllerView.songImage.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:song.localArtworkURL]];
     self.musicControllerView.songName.text = song.songTitle;
 }
