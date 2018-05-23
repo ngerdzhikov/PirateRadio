4d0dfa72c2922b2d0663a359fa2c408ad6db29ac
Nikola Gerdzhikov <gerdjikov17@abv.bg>
2018-05-23 17:48:14 +0300

    Adds history icon when displaying history cell

--- PirateRadio/ViewControllers/SearchSuggestionsTableViewController.m
+++ PirateRadio/ViewControllers/SearchSuggestionsTableViewController.m
@@ -16,8 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -28,12 +26,10 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
     return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
     if (self.delegate.searchSuggestions.count < 1) {
         return self.delegate.searchHistory.count;
     }
@@ -44,9 +40,11 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"emptyCell" forIndexPath:indexPath];
     if (self.delegate.searchSuggestions.count < 1) {
         cell.textLabel.text = self.delegate.searchHistory[indexPath.row];
+        cell.imageView.image = [UIImage imageNamed:@"history_icon"];
     }
     else {
         cell.textLabel.text = self.delegate.searchSuggestions[indexPath.row];
+        cell.imageView.image = nil;
     }
     return cell;
 }
@@ -61,18 +59,6 @@
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
     return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
 }
 */
 
