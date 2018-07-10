21844cc5a361ea28364d0285af3d269f468bb827
Nikola Gerdzhikov <gerdjikov17@abv.bg>
2018-07-10 11:39:46 +0300

    Fixes KVO unobserving again

--- PirateRadio/ViewControllers/SavedMusic/MusicPlayerViewController.m
+++ PirateRadio/ViewControllers/SavedMusic/MusicPlayerViewController.m
@@ -65,6 +65,9 @@
 
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
+}
+
+- (void)dealloc {
     [self.currentItem removeObserver:self forKeyPath:@"status"];
 }
 
