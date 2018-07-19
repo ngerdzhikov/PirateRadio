30a22aee7c6f60814cec1f98a14926715e67c5cd
Nikola Gerdzhikov <gerdjikov17@abv.bg>
2018-07-19 15:37:17 +0300

    removes CoreData files from project

--- PirateRadio/AppDelegate.m
+++ PirateRadio/AppDelegate.m
@@ -15,7 +15,6 @@
 #import "DropBox.h"
 #import "Realm.h"
 #import "ArtworkDownload.h"
-#import <ObjectiveDropboxOfficial/ObjectiveDropboxOfficial.h>
 @import AVFoundation;
 
 @interface AppDelegate ()
@@ -41,7 +40,12 @@
                               withIntermediateDirectories:YES
                                                attributes:nil
                                                     error:&error];
-    
+    NSURL *profileImagesDirectory = [NSFileManager.defaultManager URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask][0];
+    profileImagesDirectory = [profileImagesDirectory URLByAppendingPathComponent:@"profile images"];
+    [[NSFileManager defaultManager] createDirectoryAtPath:profileImagesDirectory.relativePath
+                              withIntermediateDirectories:YES
+                                               attributes:nil
+                                                    error:&error];
     NSError *setCategoryError = nil;
     [AVAudioSession.sharedInstance setCategory: AVAudioSessionCategoryPlayback error: &setCategoryError];
     
@@ -170,54 +174,6 @@
     return NO;
 }
 
-#pragma mark - Core Data stack
-
-@synthesize persistentContainer = _persistentContainer;
-
-- (NSPersistentContainer *)persistentContainer {
-    // The persistent container for the application. This implementation creates and returns a container, having loaded the store for the application to it.
-    @synchronized (self) {
-        if (_persistentContainer == nil) {
-            _persistentContainer = [[NSPersistentContainer alloc] initWithName:@"Model"];
-            [_persistentContainer loadPersistentStoresWithCompletionHandler:^(NSPersistentStoreDescription *storeDescription, NSError *error) {
-                if (error != nil) {
-                    // Replace this implementation with code to handle the error appropriately.
-                    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                    
-                    /*
-                     Typical reasons for an error here include:
-                     * The parent directory does not exist, cannot be created, or disallows writing.
-                     * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                     * The device is out of space.
-                     * The store could not be migrated to the current model version.
-                     Check the error message to determine what the actual problem was.
-                     */
-                    NSLog(@"Unresolved error %@, %@", error, error.userInfo);
-                    abort();
-                }
-            }];
-        }
-    }
-    
-    return _persistentContainer;
-}
-
-#pragma mark - Core Data Saving support
-
-- (void)saveContext {
-    NSManagedObjectContext *context = self.persistentContainer.viewContext;
-    NSError *error = nil;
-    if ([context hasChanges] && ![context save:&error]) {
-        // Replace this implementation with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        NSLog(@"Unresolved error %@, %@", error, error.userInfo);
-        abort();
-    }
-}
-
-- (NSManagedObjectContext *)managedObjectContext {
-    return self.persistentContainer.viewContext;
-}
 
 
 @end
